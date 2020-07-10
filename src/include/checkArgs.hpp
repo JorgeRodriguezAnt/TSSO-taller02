d1d956d5b9acd465e9a0723ad63cd8c4e048a211
JorgeRodriguezAnt <33755298+JorgeRodriguezAnt@users.noreply.github.com>
2020-07-10 10:44:44 -0400

    Update checkArgs.hpp

--- src/include/checkArgs.hpp
+++ src/include/checkArgs.hpp
@@ -8,51 +8,53 @@ extern int optind, opterr, optopt;
 
 class checkArgs {
 private:
-        
+        // 1) Modificar esta sección
         const std::string optString = "N:t:l:L:h";
 
         const std::string opciones = "-N <nro> -t <nro> -l <nro> -L <nro> [-h]";
+
         const std::string descripcion  = "Descripción:\n"
-                                             "\t-N   Tamaño del Vector\n"
-                                             "\t-t   Cantidad de threads a utilizar\n"
-                                             "\t-l   Limite inferior rango aleatorio\n"
-                                             "\t-L   Limite superior rango aleatorio\n"
-                                             "\t-h   Muestra esta ayuda y termina\n";
+                                         "\t-N   Tamaño del arreglo\n"
+                                         "\t-t   Cantidad de threads a utilizar\n"
+                                         "\t-l   Limite inferior rango aleatorio\n"
+                                         "\t-L   Límite superior rango aleatorio\n"
+                                         "\t[-h]   Muestra esta ayuda y termina\n"
+                                         ;
 
         typedef struct args_t{
-                uint32_t tamProblema;
+                uint32_t tamArray;
                 uint32_t numThreads;
-                uint32_t limInferior;
-                uint32_t limSuperior;
+                uint32_t limitinf;
+                uint32_t limitsup;
+
         } args_t;
 
-	
-        
+        // 2) Modificar constructor
+        // 3) Modificar ciclo "getopt" en método checkArgs::getArgs()
+        // 4) Recuerde que para compilar nuevamente, y por tratarse
+        //    de un archivo header, debe hacer primero un make clean
+
         args_t  parametros;
 
         int argc;
         char **argv;
 
 
-public:
-        checkArgs(int _argc , char **_argv);
-        ~checkArgs();
-        args_t getArgs();
+        public:
+            checkArgs(int _argc , char **_argv);
+            ~checkArgs();
+            args_t getArgs();
 
-private:
-        void printUsage();
-
-
-};
+        private:
+            void printUsage();
 
 
-
-checkArgs::checkArgs(int _argc , char **_argv){
-        parametros.tamProblema = 0;
+        };
+        checkArgs::checkArgs(int _argc , char **_argv){
+        parametros.tamArray    = 0;
         parametros.numThreads  = 0;
-        parametros.limInferior = 0;
-        parametros.limSuperior = 0;
-
+        parametros.limitinf    = 0;
+        parametros.limitsup    = 0;
         argc = _argc;
         argv = _argv;
 
@@ -62,24 +64,22 @@ checkArgs::~checkArgs(){
 
 }
 
-
 checkArgs::args_t checkArgs::getArgs(){
         int opcion;
 
         while ((opcion = getopt (argc, argv, optString.c_str())) != -1){
                 switch (opcion) {
                         case 'N':
-                                        parametros.tamProblema = atoi(optarg);
+                                        parametros.tamArray = atoi(optarg);
                                         break;
                         case 't':
                                         parametros.numThreads = atoi(optarg);
                                         break;
-
                         case 'l':
-                                        parametros.limInferior = atoi(optarg);
+                                        parametros.limitinf  = atoi(optarg);
                                         break;
                         case 'L':
-                                        parametros.limSuperior = atoi(optarg);
+                                        parametros.limitsup = atoi(optarg);
                                         break;
                         case 'h':
                         default:
@@ -88,12 +88,13 @@ checkArgs::args_t checkArgs::getArgs(){
                 }
         }
 
-	
-	
-	 if ( parametros.tamProblema == 0 ||
-                 parametros.numThreads  == 0 ){
-                printUsage();
-                exit(EXIT_FAILURE);
+
+if ( parametros.tamArray == 0 ||
+     parametros.numThreads  == 0 ||
+     parametros.limitsup == 0  ||
+     parametros.limitinf == 0){
+     	printUsage();
+        exit(EXIT_FAILURE);
         }
 
         return(parametros);
@@ -107,3 +108,5 @@ void checkArgs::printUsage(){
 
 
 #endif
+
+
