bc223c75f17a0a3cc1f40148187f7af07a0bd142
JorgeRodriguezAnt <33755298+JorgeRodriguezAnt@users.noreply.github.com>
2020-07-10 10:30:26 -0400

    Update main.cc

--- src/main.cc
+++ src/main.cc
@@ -2,84 +2,108 @@
 #include <checkArgs.hpp>
 
 void sumaParcial(const std::vector<uint32_t> &v,
-                  std::vector<uint32_t> &sumasParciales,
-                  size_t pos,
-                  size_t beginIndex,
-                  size_t endIndex) {
+                 uint32_t &suma,
+                 size_t beginIndex,
+                                 size_t endIndex) {
+    suma = 0;
     for (size_t i = beginIndex; i < endIndex; ++i){
-        sumasParciales[pos] += v[i];
+        suma += v[i];
     }
 }
 
 int main(int argc, char** argv){
 
-        uint32_t totalElementos;
-        uint32_t numThreads;      //cantidad de hilos
+        uint32_t cantArray;
+        uint32_t numThreads;
+        uint32_t limitinferior;
         uint32_t limSuperior;
-        uint32_t limInferior;
-  uint32_t sumaSerial = 0;
-        uint32_t sumaThreads = 0;
-        std::vector<uint32_t> v;
-        std::vector<std::thread*> threads;
-        std::vector<uint32_t> sumasParciales;
-        std::shared_ptr<checkArgs> argumentos = (std::shared_ptr<checkArgs>) new checkArgs(argc, argv);
-        totalElementos = argumentos->getArgs().tamProblema;   //tamaño del vector
-        numThreads     = argumentos->getArgs().numThreads;
-        limSuperior    = argumentos->getArgs().limSuperior;
-        limInferior    = argumentos->getArgs().limInferior;
-        static thread_local std::random_device device;
-        static thread_local std::mt19937 rng(device());
-        std::uniform_int_distribution<uint32_t> nRandom(limInferior, limSuperior);
 
-        for(size_t i=0; i < totalElementos; i++){
+        uint32_t sumaSerial = 0;
+      
+        uint32_t sumaThreads = 0;
+
+        std::vector<uint32_t> v;
+
+        std::shared_ptr<checkArgs> argumentos = (std::shared_ptr<checkArgs>) new checkArgs(argc, argv);
+
+        cantArray = argumentos->getArgs().tamProblema;
+        numThreads     = argumentos->getArgs().numThreads;
+        limitinferior  = argumentos->getArgs().limitinf;
+        limSuperior    = argumentos->getArgs().limitsup;
+
+        std::cout << "Tamaño del arreglo: " << cantArray << std::endl;
+        std::cout << "Threads  : " << numThreads     << std::endl;
+        std::cout << "Limit inferior: " << limitinferior  << std::endl;
+        std::cout << "Limit superior : " << limSuperior     << std::endl;
+
+         static thread_local std::random_device device;
+        static thread_local std::mt19937 rng(device());
+
+        //Modulo llenado
+        std::uniform_int_distribution<uint32_t> nRandom(limitinferior, limSuperior);
+        for(size_t i=0; i < cantArray; i++){
                 uint32_t number = nRandom(rng);
                 v.push_back( number );
-                sumasParciales.push_back(0);
         }
 
-      //SERIAL
-
-  auto start = std::chrono::system_clock::now();
+        //======SERIAL======
+        auto start = std::chrono::high_resolution_clock::now();
         for(auto& num : v){
                 sumaSerial += num;
         }
-  auto end = std::chrono::system_clock::now();
-        std::chrono::duration<float,std::milli> duration = end - start;
-        auto totalTimeSerial = duration.count();
 
-        //HILOS
 
-        for(size_t i = 0; i < numThreads; ++i){
-                threads.push_back( new std::thread(sumaParcial,
-                                                                std::ref(v), std::ref(sumasParciales),i,
-                                                                i*(totalElementos)/numThreads,
-                                                                (i+1)*(totalElementos)/numThreads));
+
+        auto end = std::chrono::high_resolution_clock::now();
+        auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        float totalTimeSerial = elapsed.count();
+
+        //======THREADS======
+        std::thread hilos[numThreads];
+        uint32_t sumasParciales[numThreads];
+        uint32_t tam = v.size() / numThreads;
+
+        //Separación del trabajo
+
+        start = std::chrono::high_resolution_clock::now();
+        for(uint8_t x = 0; x<numThreads;x++){
+                hilos[x] = std::thread(sumaParcial, std::ref(v), std::ref(sumasParciales[x]), tam*x, tam*(x+1));
         }
-  start = std::chrono::system_clock::now();
-        for(auto& th : threads){
-                th->join();   //sincronizar hilos
+        for(uint8_t x = 0; x<numThreads;x++){
+                hilos[x].join(); // se sincronizan los hilos
         }
 
-        // Reducción (Consolidación de resultados parciales)
-        for(auto& sumaTh : sumasParciales){
-                sumaThreads += sumaTh;
+        //Consolidación de resultados parciales
+        for(uint8_t x = 0; x<numThreads;x++){
+                sumaThreads+= sumasParciales[x];
         }
-  end = std::chrono::system_clock::now();
-  std::chrono::duration<float,std::milli> duration2 = end - start;
-  auto totalTimeThread = duration2.count();
 
-        //DESEMPEÑO
-  float speedUp = (totalTimeSerial/totalTimeThread);
-  float eficiencia = 1/(1+(totalTimeThread/totalTimeSerial));
-      
-	//RESULTADOS
-  std::cout << "SumaSerial:" << sumaSerial << std::endl;
-  std::cout << "TiempoTotalHilos:" << totalTimeThread << std::endl;
-  std::cout << "TiempoTotalSerial:" << totalTimeSerial << std::endl;
-  std::cout << "Acceleracion:" <<speedUp << std::endl;
-  std::cout << "Eficiencia:" <<eficiencia << std::endl;
+        end = std::chrono::high_resolution_clock::now();
+        elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        float totalTimeThread = elapsed.count();
+
+        //======Mostrar resultados=====
+        float speedUp = totalTimeSerial/totalTimeThread;
+        float eficiencia = speedUp/numThreads;
+        std::cout << "====Serial====" << std::endl;
+        std::cout << "sumaSerial  : " << sumaSerial << std::endl;
+        std::cout << "T. serial   :"  << totalTimeSerial << std::endl;
+        std::cout << std::endl;
+        std::cout << "====Threads====" << std::endl;
+        std::cout << "sumaThreads : " << sumaThreads << std::endl;
+        std::cout << "T. paralelo :"  << totalTimeThread << std::endl;
+        std::cout << std::endl;
+
+        std::cout << "SpeedUp    :"<<speedUp << std::endl;
+        std::cout << "Eficiencia :"<<eficiencia << std::endl;
+        
         return(EXIT_SUCCESS);
 }
+
+
+
+
+
 
 
 
